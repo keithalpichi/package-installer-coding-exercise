2c57fb198ed9b8fde6a94f7f06e07d457ebaa4bb
Keith Alpichi <keithalpichi@gmail.com>
2017-05-31 16:06:34 -0700

    handled cases for invalid input types

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const packageInstaller = deps => {
   if (!deps) { return null }
+  if (!Array.isArray(deps)) { throw new TypeError(`Input should be an array but instead got "${deps}"`) }
   if (deps.length === 0) { return [] }
 }
 
--- test.js
+++ test.js
@@ -8,3 +8,21 @@ test('no input returns null output', t => {
 test('empty array as input returns empty array as output', t => {
   t.is(packageInstaller([]).length, 0)
 })
+
+test('should throw TypeError if input is not array', t => {
+  const error = t.throws(() => {
+    packageInstaller('this throw an error')
+  }, TypeError)
+
+  t.is(error.message, 'Input should be an array but instead got "this throw an error"')
+})
+
+test.skip('single package with no dependencies should return the single packade', t => {
+  t.is(packageInstaller(['CamelCaser: ']), 'CamelCaser')
+})
+
+test.skip('should throw an error if dependencies contain cycles', t => {
+  t.throws(() => {
+    packageInstaller(['KittenService: ', 'Leetmeme: Cyberportal', 'Cyberportal: Ice', 'CamelCaser: KittenService', 'Fraudstream: ', 'Ice: Leetmeme'])
+  }, Error)
+})
