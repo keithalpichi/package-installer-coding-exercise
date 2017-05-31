e81660b12100fe0726db636b74e06a3b9af496c1
Keith Alpichi <keithalpichi@gmail.com>
2017-05-31 16:14:57 -0700

    added/updated tests

--- test.js
+++ test.js
@@ -17,12 +17,18 @@ test('should throw TypeError if input is not array', t => {
   t.is(error.message, 'Input should be an array but instead got "this throw an error"')
 })
 
-test.skip('single package with no dependencies should return the single packade', t => {
+test.skip('single package with no dependencies should return the single package as a string', t => {
   t.is(packageInstaller(['CamelCaser: ']), 'CamelCaser')
 })
 
+test.skip('should return a valid dependency path for two packages as a string', t => {
+  t.is(packageInstaller(['KittenService: CamelCaser', 'CamelCaser: ']), 'CamelCaser, KittenService')
+})
+
 test.skip('should throw an error if dependencies contain cycles', t => {
-  t.throws(() => {
+  const error = t.throws(() => {
     packageInstaller(['KittenService: ', 'Leetmeme: Cyberportal', 'Cyberportal: Ice', 'CamelCaser: KittenService', 'Fraudstream: ', 'Ice: Leetmeme'])
   }, Error)
+
+  t.is(error.message, 'Invalid input due to cycles in the dependency path')
 })
